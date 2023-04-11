eb45c5e25a3cc749f32af091f9e0b84da1a6f68b
Lord0fGrim <115959756+Lord0fGrim@users.noreply.github.com>
2023-04-10 21:05:10 -0400

    Add files via upload

---
+++ alphabetize a list.py
@@ -0,0 +1,7 @@
+#Write a program that takes a list of strings as input and 
+#returns a new list with all the strings sorted alphabetically.
+
+my_list = ['shit', 'bitch', 'ass', 'fuck']
+my_list.sort()
+print(my_list)
+
